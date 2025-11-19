85d0104650afd35d9d20e995d6bdeda05d7ea0ea
Moulik Sharma <moulik.sharma@globallogic.com>
2025-11-19 16:21:56 +0530

    Add: ImageUrl Field in Product

---
+++ BasicCrudApp/Migrations/20251119102846_ImageUrl.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasicCrudApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+        }
+    }
+}
--- BasicCrudApp/Models/Product.cs
+++ BasicCrudApp/Models/Product.cs
@@ -5,4 +5,5 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
 }
